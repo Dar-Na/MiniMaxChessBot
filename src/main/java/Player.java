d6434abd8507cc467f28754cf87eb67580ed8953
Dar-Na <70232815+Dar-Na@users.noreply.github.com>
2022-07-07 21:09:29 +0200

    initialization

---
+++ src/main/java/Player.java
@@ -0,0 +1,9 @@
+import com.github.bhlangonijr.chesslib.move.Move;
+
+abstract public class Player {
+    GameLogic gameLogic;
+    public Player(GameLogic gameLogic){
+        this.gameLogic = gameLogic;
+    }
+    public abstract Move getMove();
+}
